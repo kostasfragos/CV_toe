8c9347105b50b882cd14761dee6abf73901c4d84
unknown <you@example.com>
2023-11-18 11:38:41 +0200

    Tennis

--- game.py
+++ game.py
@@ -25,6 +25,76 @@ def play(board, position, symbol):
     board[position] = symbol
     return True
 
+def check_win(board):
+    if (board[0] == board[1] == board[2]) and board[0]!=KENO:
+        return board[0]
+    if (board[3] == board[4] == board[5]) and board[3]!=KENO:
+        return board[3]
+    if (board[6] == board[7] == board[8]) and board[6]!=KENO:
+        return board[6]
+    if (board[0] == board[3] == board[6]) and board[0]!=KENO:
+        return board[0]
+    if (board[1] == board[4] == board[7]) and board[1]!=KENO:
+        return board[1]
+    if (board[2] == board[5] == board[8]) and board[2]!=KENO:
+        return board[2]
+    if (board[0] == board[4] == board[8]) and board[0]!=KENO:
+        return board[0]
+    if (board[2] == board[4] == board[6]) and board[2]!=KENO:
+        return board[2]
+    return KENO 
+
+lines = [
+    (0, 1, 2),
+    (3, 4, 5),
+    (6, 7, 8),
+    (0, 3, 6),
+    (1, 4, 7),
+    (2, 5, 8),
+    (0, 4, 8),
+    (2, 4, 6)
+]
+
+def ai_play(board, symbol):
+
+    # επίθεση
+    for line in lines:
+        count_s = 0 
+        for pos in line:
+            if board[pos] == symbol:
+                count_s += 1
+                
+        if count_s == 2:
+            hole = 0
+            for pos in line:
+                if board[pos] == KENO:
+                    hole = pos
+                    break
+                
+            play(board, hole, symbol)
+            break
+
+    # άμυνα
+    enemy_symbol = -1 # ?????
+    for line in lines:
+        count_s = 0 
+        for pos in line:
+            if board[pos] == enemy_symbol:
+                count_s += 1
+                
+        if count_s == 2:
+            hole = 0
+            for pos in line:
+                if board[pos] == KENO:
+                    hole = pos
+                    break
+                
+            play(board, hole, symbol)
+            break
+
+    # tyxaio
+
+
 # play(game_list, 0, X)
 # play(game_list, 4, X)
 # play(game_list, 5, X)
@@ -32,19 +102,32 @@ def play(board, position, symbol):
 # play(game_list, 7, O)
 # play(game_list, 8, O)
 
+
 turn = X
 for i in range(9):
     display_board(game_list)
 
-    pos = int(input("Που θες να παίξεις;"))
-    #Όσο δεν δίνεται σωστή θέση ξαναδοκιμάζουμε
-    while play(game_list, pos, turn) == False:
-        pos = int(input("Λάθος θέση. Δοκίμασε ξανά:"))
+    if i == 4:
+        ai_play(game_list, turn)
+    else:
+        pos = int(input("Που θες να παίξεις;"))
+        # Όσο δεν δίνεται σωστή θέση ξαναδοκιμάζουμε
+        while play(game_list, pos, turn) == False:
+            pos = int(input("Λάθος θέση. Δοκίμασε ξανά:"))
 
-    # Κώδικας αλλαγής σειράς
+   
+    winner = check_win(game_list)
+
+    if winner!=KENO:
+        break
+
+     # Κώδικας αλλαγής σειράς
     if turn==X:
         turn = O
     else:
         turn = X
-        
+
 display_board(game_list)
+
+if winner!=KENO:
+    print("Νίκησε ο", winner) 
